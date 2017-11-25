c4f830f863a338a6cbb2846714f0a4d4e52c589a
unknown <camiloignacio.parraguezpastenes.es>
2017-11-25 13:00:09 -0300

    .

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,12 +5,14 @@ import { EstadisticasComponent } from './estadisticasComponent/estadisticas.comp
 import { MapaTiempoRealComponent } from './mapasComponent/mapaTiempoReal/mapaTiempoReal.component';
 import { MapaHistoricoComponent } from './mapasComponent/mapaHistorico/mapaHistorico.component'
 import { HomeComponent } from './homeComponent/home.component';
+import { CuestionarioComponent } from './cuestionarioComponent/cuestionario.component';
 
 const routes: Routes = [
 	{path: '', component: HomeComponent},
 	{path: 'estadisticas', component: EstadisticasComponent},
 	{path: 'mapaTiempoReal', component: MapaTiempoRealComponent},
-	{path: 'mapaHistorico', component: MapaHistoricoComponent}
+	{path: 'mapaHistorico', component: MapaHistoricoComponent},
+	{path: 'cuestionario', component: CuestionarioComponent}
 ];
 
 
--- src/app/estadisticasComponent/estadisticas.component.ts
+++ src/app/estadisticasComponent/estadisticas.component.ts
@@ -14,57 +14,59 @@ import { Observable } from 'rxjs/Observable';
 })
 
 export class EstadisticasComponent implements OnInit{
-
-
+	
+	
 	private isDataAvailableMensual: boolean = false;
 	private isDataAvailableAnual: boolean = false;
-
+	
 	private accidentes: Array<Observable<Object>>;
 	
-	private selectedValue= 'Mensual';
+	private selectedValue= 'Anual';
 	private graficos = [{ value: 'Anual', viewValue: 'Anual' },
-										{ value: 'Mensual', viewValue: 'Mensual' }];
-
+	{ value: 'Mensual', viewValue: 'Mensual' }];
+	
 	public barChartOptionsAnual: any = {
 		scaleShowVerticalLines: true,
-		responsive: true,
-		
+		responsive: true,		
 	};
-	 public barChartLabelsAnual: 	string[] = [];
-	 public barChartTypeAnual:		string = 'bar';
-	 public barChartLegendAnual: 	boolean = true;
 	
-	 public barChartDataAnual:any[] = [
-		 { 
-			 data: [], 
-			 label: 'Numero de accidentes de tránsito', 
-			 
-		},
-	];
 	
 
-	private ene = true;
-	private feb = true;
-	private mar = true;
-	private abr = true;
-	private may = true;
-	private jun = true;
-	private jul = true;
-	private ago = true;
-	private sep = true;
-	private oct = true;
-	private nov = true;
-	private dic = true;
+	public barChartLabelsAnual: 	string[] = [];
+	public barChartTypeAnual:		string = 'bar';
+	public barChartLegendAnual: 	boolean = true;
+	 
+	public barChartDataAnual:any[] = [
+		 { 
+			 data: [], 
+			 label: 'Número de accidentes de tránsito', 
+			 
+			},
+		];
+	// variable que va a determinar los años
+	public graficoDataAnual: Array<boolean> = [];
+
+	public Colors: Array<any> = [
+		{ // blue
+			backgroundColor: 'rgba(51, 9, 179,0.6)',
+			borderColor: 'rgba(51, 9, 179,1)',
+			pointBackgroundColor: 'rgba(51, 9, 1790,1)',
+			pointBorderColor: '#fff',
+			pointHoverBackgroundColor: '#fff',
+			pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+	}];
 
 
 	public barChartOptionsMensual: any = {
 		scaleShowVerticalLines: false,
 		responsive: true
 	};
-	public barChartLabelsMensual: string[] = [];
+	public barChartLabelsMensual: string[] = ['Enero','Febrero','Marzo','Abril','Mayo'
+											 ,'Junio','Julio','Agosto','Septiembre'
+											 ,'Octubre','Noviembre','Diciembre'];
+
 	public barChartTypeMensual: string = 'bar';
 	public barChartLegendMensual: boolean = true;
-	
 	public barChartDataMensual: Array<any> = [{data:[0,0,0,0,0,0,0,0,0,0,0,0], label: '' }];
 	 
 	constructor(
@@ -78,8 +80,9 @@ export class EstadisticasComponent implements OnInit{
 	ngOnInit() {
 
 		this.titleService.setTitle('Estadísticas - OTRIT');
-
+// datos anuales
 		this.updateGraficoAnual();
+// datos mensuales
 		this.updateGraficoMensual();
 		
 	}
@@ -91,7 +94,7 @@ export class EstadisticasComponent implements OnInit{
 			this.editarGraficoAnual(this.accidentes);
 		});
 
-		this.editarLabel();
+		// this.editarLabel();
 	}
 
 	private updateGraficoMensual(){
@@ -101,6 +104,7 @@ export class EstadisticasComponent implements OnInit{
 		});
 	}
 
+/*
 	private editarLabel(){
 		console.log(this.ene);
 
@@ -144,6 +148,7 @@ export class EstadisticasComponent implements OnInit{
 	
 		this.barChartLabelsMensual = meses;
 	}
+*/
 
 
 	
@@ -156,7 +161,7 @@ export class EstadisticasComponent implements OnInit{
 		// Contador de años
 		let indiceAnio = 0;
 		
-		this.barChartDataMensual[indiceAnio]['label'] = 'Cantidad accidentes ' + accidentes[0]['ano'];
+		this.barChartDataMensual[indiceAnio]['label'] = 'Cantidad accidentes año ' + accidentes[0]['ano'] + '\t' ;
 	
 		
 		for(let i = 0; i< accidentes.length; i++){
@@ -170,7 +175,7 @@ export class EstadisticasComponent implements OnInit{
 				indiceAnio ++;
 				this.barChartDataMensual[indiceAnio] = [];				
 				this.barChartDataMensual[indiceAnio]['data'] = [0,0,0,0,0,0,0,0,0,0,0,0];
-				this.barChartDataMensual[indiceAnio]['label'] = 'Cantidad accidentes ' + accidentes[i]['ano'];
+				this.barChartDataMensual[indiceAnio]['label'] = 'Cantidad accidentes año ' + accidentes[i]['ano'];
 			}			
 
 			this.barChartDataMensual[indiceAnio]['data'][+accidentes[i]['mes']-1] = (+ this.accidentes[i]['cantidadMes']);
@@ -189,16 +194,21 @@ export class EstadisticasComponent implements OnInit{
 		for (let i = 0; i< accidentes.length; i++){
 			cantAccidentes.push(+ this.accidentes[i]['cantidadAno']);
 			labelAccidentes.push(this.accidentes[i]['ano']);
+
+			this.graficoDataAnual[i] = true;
 		}
 		this.barChartDataAnual[0]['data'] = cantAccidentes;
 		this.barChartLabelsAnual = labelAccidentes;
 		
 		this.isDataAvailableAnual = true;
+
+		console.log(cantAccidentes.length + ' Cantidad de años');
 	}
 
 	descargaDatos(){
 		console.log('descargando datos');
-
 		this.servicio.descargaExcel(this.barChartDataMensual);
 	}
+
+
 }
